f6d7e4271e4ad2ae679364dded867535b48bee7b
zzz-x <2268189794@qq.com>
2024-04-19 09:32:21 +0800

    Project 0 : finishi trie store
    pass trie_store_test
    
    Signed-off-by: zzz-x <2268189794@qq.com>

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -3,6 +3,8 @@
 
 namespace bustub {
 
+// one writer, multiple reader
+// 即使有一个进程在写，别的进程可以基于原来的root读
 template <class T>
 auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // Pseudo-code:
@@ -11,20 +13,41 @@ auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
   // (2) Lookup the value in the trie.
   // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
   //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
+
+  // 这里加锁是为了是防止拿root的时候有写操作
+  root_lock_.lock();
+  Trie query_root = root_;
+  root_lock_.unlock();
+
+  auto value_ptr = query_root.Get<T>(key);
+
+  if (!value_ptr) {
+    return std::nullopt;
+  }
+  return ValueGuard<T>(query_root, *value_ptr);
 }
 
 template <class T>
 void TrieStore::Put(std::string_view key, T value) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
+  this->write_lock_.lock();
+  Trie modified_root = root_.Put(key, std::move(value));
+  this->root_lock_.lock();
+  root_ = modified_root;
+  this->root_lock_.unlock();
+  this->write_lock_.unlock();
 }
 
 void TrieStore::Remove(std::string_view key) {
   // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
   // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
+  this->write_lock_.lock();
+  Trie modified_root = root_.Remove(key);
+  this->root_lock_.lock();
+  root_ = modified_root;
+  this->root_lock_.unlock();
+  this->write_lock_.unlock();
 }
 
 // Below are explicit instantiation of template functions.
